3c9327ca0e8c71794471a3f000b2713a4b1ffdb4
EffaOjah <effaojah@gmail.com>
2026-07-05 13:17:30 +0100

    Minor change

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api', // Update if your backend runs on a different port
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
 
 // Intercept requests to add the Authorization header if it exists
